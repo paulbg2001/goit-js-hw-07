33597839def0276392314e1d53e758a4e21fcc77
paulbg2001 <paul.baragraeff@ulbsibiu.ro>
2023-12-03 17:40:07 +0200

    s

---
+++ src/js/02-lightbox.js
@@ -0,0 +1,20 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const photosMarkup = createGalleryItem(galleryItems);
+function createGalleryItem(element) {
+  return element
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>`;
+    })
+    .join("");
+}
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+const galleryhandler = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+galleryhandler.options("show.simpleligthbox");
